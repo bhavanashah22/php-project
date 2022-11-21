b42ccaa6b0c147a718a3cd8b7f26443010d33a44
Hemendra <hemubirla@gmail.com>
2022-11-21 11:06:44 +0530

    my create teble shop

--- db.php
+++ db.php
@@ -2,15 +2,15 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-//$dbname  = "project_c";
-$conn = new mysqli($servername, $username, $password,);
+$dbname  = "fileproject";
+$conn = new mysqli($servername, $username, $password, $dbname);
 /*if($conn->connect_error){
 	die("database not connected". $conn->connect_error);
 }
 else {
 	echo "database connected";
 }
-*/
+
 $sql = "CREATE DATABASE  fileproject";
 if($conn->query($sql) == true){
    echo "database crteated";
@@ -19,4 +19,31 @@ else {
 
 	die("databse not created". $conn->error);
 }
+*/
+
+$sql = "CREATE TABLE my_shop (
+id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+proname VARCHAR(30) NOT NULL,
+proquntiy VARCHAR(30) NOT NULL,
+prointre VARCHAR(50),
+reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+)";
+
+if ($conn->query($sql) === TRUE) {
+  echo "Table MyGuests created successfully";
+} else {
+  echo "Error creating table: " . $conn->error;
+}
+
+//my teble create//
+
+
+
+
+
+
+
+
+
+
 //my name is  bhawana //
